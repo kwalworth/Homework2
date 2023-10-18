95118d5beecb2f1fac3f6e7c786ae2e5b3c2b29b
kwalworth <kwwalworth@gmail.com>
2023-10-18 17:38:15 -0500

    "Reduce Speed"

--- my_package/my_package/my_node.py
+++ my_package/my_package/my_node.py
@@ -403,7 +403,7 @@ class RandomWalk(Node):
             DEGREES_OBJECTIVE = 180
             
             if((DEGREES_OBJECTIVE - DEGREES) < DEGREES_OBJECTIVE/2):
-                ANGULAR_VELOCITY = ANGULAR_VELOCITY/3
+                ANGULAR_VELOCITY = ANGULAR_VELOCITY/5
                 if ANGULAR_VELOCITY < 0.01:
                     ANGULAR_VELOCITY = 0.01
             elif(ANGULAR_VELOCITY < target_angular_velocity):
@@ -447,7 +447,7 @@ class RandomWalk(Node):
             DEGREES_OBJECTIVE = 360
             
             if((DEGREES_OBJECTIVE - DEGREES) < DEGREES_OBJECTIVE/2):
-                ANGULAR_VELOCITY = ANGULAR_VELOCITY/3
+                ANGULAR_VELOCITY = ANGULAR_VELOCITY/5
                 if ANGULAR_VELOCITY < 0.01:
                     ANGULAR_VELOCITY = 0.01
             elif(ANGULAR_VELOCITY < target_angular_velocity):
