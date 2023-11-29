5aa18d22abb7ddcfc8cc0ad16bf18af9149bac4c
kwalworth <116377367+kwalworth@users.noreply.github.com>
2023-11-29 11:00:31 -0600

    speed

--- my_package/my_package/my_node.py
+++ my_package/my_package/my_node.py
@@ -15,7 +15,7 @@ import random
 
 
 
-LINEAR_VEL = 0.1
+LINEAR_VEL = 0.12
 STOP_DISTANCE = 1
 LIDAR_ERROR = 0.1
 LIDAR_AVOID_DISTANCE = 0.7
@@ -177,7 +177,7 @@ class RandomWalk(Node):
             #print(f"Random Number: {random_number}")
             #if(random_number != 1):
             print(f"COUNT: {COUNT}")
-            if(COUNT != 75):
+            if(COUNT != 73):
                 self.movingForward(front_lidar_min)
                 COUNT = COUNT + 1
             else:
